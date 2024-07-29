ccee1b5783388f00f66501fb5a9fe68d3bcb0062
sai_vara_1729 <144797272+saivara1341@users.noreply.github.com>
2024-07-29 18:34:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay');
+const menu = document.getElementById('mobile-menu');
+const counters = document.querySelectorAll('.counter');
+let scrollStarted = false;
+
+btn.addEventListener('click', navToggle);
+document.addEventListener('scroll', scrollPage);
+
+function navToggle() {
+  btn.classList.toggle('open');
+  overlay.classList.toggle('overlay-show');
+  document.body.classList.toggle('stop-scrolling');
+  menu.classList.toggle('show-menu');
+}
+
